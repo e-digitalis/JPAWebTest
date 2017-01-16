78dc2e4caf0803ee153e55b636591f3870f3a3ce
User <User@FDC7M82>
2017-01-16 16:33:37 +0100

    Learning JPA

---
+++ src/java/Model/ManufacturerFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author User
+ */
+@Stateless
+public class ManufacturerFacade extends AbstractFacade<Manufacturer> {
+
+    @PersistenceContext(unitName = "WebTestPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ManufacturerFacade() {
+        super(Manufacturer.class);
+    }
+    
+}
---
+++ src/java/Model/MicroMarketFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author User
+ */
+@Stateless
+public class MicroMarketFacade extends AbstractFacade<MicroMarket> {
+
+    @PersistenceContext(unitName = "WebTestPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public MicroMarketFacade() {
+        super(MicroMarket.class);
+    }
+    
+}
